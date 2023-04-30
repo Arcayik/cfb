95797b9bb28f3be548f70426089cb62f1d4ac8cd
Blakely Haughton <blakely.haughton@gmail.com>
2023-04-30 11:22:34 -0400

    added graceful ctrl-c exit handling

--- src/record.rs
+++ src/record.rs
@@ -3,6 +3,10 @@ use std::time::Instant;
 use std::fs::OpenOptions;
 use std::io::Write;
 
+// Ctrl-c Handling
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+
 pub fn capture(fbdev: &str, path: &str) -> Result<(), std::io::Error> {
     // Initialize Framebuffer struct
     let framebuffer = Framebuffer::new(fbdev).unwrap();
@@ -25,8 +29,17 @@ pub fn capture(fbdev: &str, path: &str) -> Result<(), std::io::Error> {
     // Initialize memory buffer
     let mut frame: Vec<u8> = vec![0u8; (line_length * h) as usize];
 
+    // Initialize exit handling variables
+    let running = Arc::new(AtomicBool::new(true));
+    let r = running.clone();
+    // Set Exit Handler
+    ctrlc::set_handler(move || {
+        r.store(false, Ordering::SeqCst);
+    }).expect("Error setting Ctrl-c handler");
+
     // Loop to collect frame data as fast as possible
-    for i in 1..=30 {
+    let mut framenum = 1;
+    while running.load(Ordering::SeqCst) {
         let start = Instant::now();
         frame.clear();
         let frame = framebuffer.read_frame();
@@ -34,9 +47,12 @@ pub fn capture(fbdev: &str, path: &str) -> Result<(), std::io::Error> {
         outfile.write_all(&frame)?;
 
         let time: f32 = start.elapsed().as_secs_f32();
-        println!("{}: {:?}s", i, time);
+        println!("{}: {:?}s", framenum, time);
 
         outfile.write_all(&time.to_le_bytes())?;
+        framenum += 1;
     }
+    println!("Exit signal recieved, recording stopped.");
+
     Ok(())
 }
